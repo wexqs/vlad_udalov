331b28c667c274874702496d3905c875cb4a0077
wexqs <pufik2901@mail.ru>
2024-10-14 17:47:38 +0300

    Update modAlphaCipher.cpp

--- lab_2/modAlphaCipher.cpp
+++ lab_2/modAlphaCipher.cpp
@@ -1,1 +1,56 @@
+#include "Udalov.h"
 
+string modAlphaCipher::decoder(const std::string& cipher_st) // РАСШИФРОВАНИЕ
+{
+    string s = getValidText(cipher_st);
+    string n_s = s;
+    int dlina, nstrok, position, a; // dlina-длина строки ; nstrok-количество строк ; position-новая позиция символа, который меняем ;
+    dlina = s.size(); // st-строка с текстом, который вводится пользователем
+    nstrok = (dlina - 1) / key + 1; // key-количество столбцов в таблице
+    a = 0;
+    for (int colum_number = key; colum_number > 0; colum_number--) { // colum_number-номер столбца
+        for (int line_number = 0; line_number < nstrok; line_number++) { // line_number-номер строки
+            position = key*line_number+colum_number; //
+            if (position-1 < dlina) {
+                n_s[position-1] = s[a];
+                a++;
+            }
+        }
+    }
+    return n_s;
+}
+string modAlphaCipher::coder(const string& open_st) // ЗАШИФРОВАНИЕ
+{
+    string s = getValidText(open_st);
+    string n_s = s;
+    int dlina, nstrok, position, a; // dlina-длина строки ; nstrok-количество строк ; position-новая позиция символа, который меняем ;
+    dlina = s.size(); // st-строка с текстом, который вводится пользователем
+    nstrok = (dlina - 1) / key + 1; //key-количество столбцов в таблице
+    a = 0;
+    for (int colum_number = key; colum_number > 0; colum_number--) { // colum_number-номер столбца
+        for (int line_number = 0; line_number < nstrok; line_number++) { // line_number-номер строки
+            position = colum_number+key*line_number;
+            if (position-1 < dlina) {
+                n_s[a] = open_st[position-1];
+                a++;
+            }
+        }
+    }
+    return n_s;
+}
+
+inline std::string modAlphaCipher::getValidText(const std::string & s)
+{
+    string tmp;
+    for (auto c:s) {
+        if (isalpha (c)) {
+            if (islower (c))
+                tmp.push_back(toupper(c));
+                else
+                    tmp.push_back(c);
+    }
+}
+if (tmp.empty())
+    throw cipher_error ("Empty open text");
+    return tmp;
+}
